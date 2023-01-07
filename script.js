f60a51454cd50b195a82f3174d382a18e0655660
erika helm <erikadphelm16@gmail.com>
2023-01-06 18:11:23 -0600

    gettin there

--- script.js
+++ script.js
@@ -2,43 +2,74 @@
 
 const cards = document.querySelectorAll(".card")
 const message = document.querySelector(".msg")
+const playButton = document.querySelector(".playButton") 
+
 //define variable to a boolean to check if cards are flipped
-//define variable to a boolean to 
+//define variable to a boolean for the same card not to register as a match
 //define card variables to represent the click of each
+
+
+playButton.addEventListener("click", switchPage)
+
+
+function switchPage (){
+    const cards = document.querySelector(".hide")
+    if (cards.style.display !== "none"){
+        cards.style.display = "none"
+    }else{
+        cards.style.display = "block"
+    }
+    // console.log("worked")
+}
+
+
+
+
+
+
+
+
+
+
+
 
 const cardsArray = Array.prototype.slice.call(cards)
 
+for (let i = 0; i < cardsArray.length; i++){
+    cardsArray[i].addEventListener("click", flipCard)
+}
 
-
+chosenCards = []
 
 let flippedCard = false
 let lockBoard = false
-let firstCard, secondCard
+let firstCard
+let secondCard
 let endtime = "00:00"
 
 
 
-const updateCountdown = setInterval(() =>{
+// const updateCountdown = setInterval(() =>{
     
-    const startingTime = 2
-    let minutes = Math.floor(time / 60) 
-    let seconds = Math.floor(time % 60 )
-    let endtime = "00:00"
-    time--;
-    timer.innerHTML = minutes + " : " + seconds
-    if (minutes == 0 && seconds == 0){
-      clearInterval (updateCountdown)
-        timer.innerHTML = endtime
+//     const startingTime = 2
+//     let minutes = Math.floor(time / 60) 
+//     let seconds = Math.floor(time % 60 )
+//     let endtime = "00:00"
+//     time--;
+//     timer.innerHTML = minutes + " : " + seconds
+//     if (minutes == 0 && seconds == 0){
+//       clearInterval (updateCountdown)
+//         timer.innerHTML = endtime
       
        
-   }else if (minutes <= 9 && seconds <= 9) {
-     timer.innerHTML = "0"+ minutes + " : " +"0" +seconds
-   // }else if (seconds <= 9 ){
-   //   timer.innerHTML = minutes + " : " + "0"+seconds
-   } 
+//    }else if (minutes <= 9 && seconds <= 9) {
+//      timer.innerHTML = "0"+ minutes + " : " +"0" +seconds
+//    // }else if (seconds <= 9 ){
+//    //   timer.innerHTML = minutes + " : " + "0"+seconds
+//    } 
 
     
-}, 1000)
+// }, 1000)
 
 
 
@@ -47,99 +78,79 @@ const updateCountdown = setInterval(() =>{
 //create a function to flip cards
 
 function flipCard () {
-//if lockBoard = false, then check for match
-if(lockBoard){
-        checkForMatch()
-    }
+//this represents the element that was clicked 
+this.classList.toggle("flip")
 
-//if this is deeply equal to the first cards info, then check for match
-if(this === firstCard){
-    checkForMatch()
-}
-//adding .flip from css to each card
-this.classList.add("flip")
-
-//if flipped card is true, change the defined boolean to true, and the 
-    //first card will equal the info inside the div of the card clicked
 if (!flippedCard){
-    
-    flippedCard = true;
+
+    flippedCard = true
     firstCard = this
-//otherwise, the second card will equal the info inside the div of card clicked,
-    //and will check for a match of the first/second
-    }else{ 
 
-        secondCard = this 
+} else {
 
-    checkForMatch()
-    }   
+    flippedCard = false
+    secondCard = this
+checkForMatch()
+
+// console.log(firstCard)
+// console.log(secondCard)
+// console.log(flippedCard)
+// console.log(firstCard.dataset.id)
+// console.log(secondCard.dataset.id)
+
+
+
+
 }
 
 
-//create a function to check for matches
 
-function checkForMatch(){
-    //if the info inside the div of the first card matches the info inside
-        //the second card, then incoke the matching cards function
-    if (timer !== endtime)
-    if(cardsArray[0] === cardsArray[1] 
-        // cardsArray[2] === cardsArray[3] &&
-        // cardsArray[4] ===  cardsArray[5] && 
-        // cardsArray[6] ===  cardsArray[7]  
-    ){
-        document.querySelector(".msg").innerHTML ("You won!")
-    //otherwise, invoke the not matching function
-    // }else {
-    //     notMatching()
-    }
+
 }
 
-function matchingCards (){
+function checkForMatch () {
+    if (firstCard.dataset.id === secondCard.dataset.id){
+    cardsMatch()
+}else{
+    
+    cardsDontMatch()
+}
+}
+
+
+function cardsMatch () {
     firstCard.removeEventListener("click", flipCard)
     secondCard.removeEventListener("click", flipCard)
+}
+
+function checkCardsMatch () { 
+     setTimeout(() => {
     
-    document.querySelector(".msg").innerHTML = "Yes!"
-    reset()
-}
-
-
-
-
-
-function notMatching (){
-    lockBoard = true
-    setTimeout(() => { 
-        firstCard.classList.remove("flip")
-        secondCard.classList.remove("flip")
-        reset()
-        }, 700)
+    if (cardsArray[0] === cardsArray[1] &&
+        cardsArray[2] === cardsArray[3] &&
+        cardsArray[4] === cardsArray[5] &&
+        cardsArray[6] === cardsArray[7] ){
+            cardsMatch()
+          alert('got em all')
         
+        }
+ }, 100)
 }
+ 
 
-function reset (){
-    flippedCard = false
-    lockBoard = false
-    firstCard = null
-    secondCard = null
-   
+
+
+
+     
+
+
+
+function cardsDontMatch () {
+    setTimeout(() => {
+    firstCard.classList.remove("flip")
+    secondCard.classList.remove("flip")
+    }, 500)
 }
-
-(function shuffleCards(){
-    cards.forEach(card => {
-        let randomNum = Math.floor(Math.random() * 9)
-        cards.style = randomNum
-    })
-})()
-
-cards.forEach(card => card.addEventListener("click", flipCard))
-
-
-
-
-
-
-
-
 
 
 
